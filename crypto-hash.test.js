18cf9e3f0e704cc816168b244e61da727f15117d
anshugupta673 <alch.anshu@gmail.com>
2021-09-19 20:28:03 +0530

    API mod added

--- crypto-hash.test.js
+++ crypto-hash.test.js
@@ -3,5 +3,11 @@ const cryptoHash = require('./crypto-hash');
 describe('cryptoHash()', () => {
     //ensure that this fun produces SHA output rather SHA256
     //google it : SHA256 gebnerator online
-    
+    it('generates a SHA-256 hashed output', () => {
+        expect(cryptoHash('foo')).toEqual('2c26b46b6bffc68ff99b53c1db0413413422d706483bfa0f98a5e886266e7ae');
+    });
+
+    if('it produces the same hash with the same arguments in any order', () => {
+        expect(cryptoHash('one', 'two', 'three')).toEqual(cryptoHash('three', 'two', 'one'));
+    });
 });
